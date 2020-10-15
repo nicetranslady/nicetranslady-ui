2c36e45dc81910bcba2993aceaa3d84801cf7482
nicetranslady <email@nicetranslady.online>
2020-10-15 17:57:22 +0100

    first commit

---
+++ nicetranslady-ui/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nicetranslady.ui.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+    // Define the SocialMediaCompany class
+    public class SocialMediaCompany
+    {
+        public string Name { get; set; }
+        public string HomeURL { get; set; }
+        public string Prefix { get; set; }
+    }
+}
